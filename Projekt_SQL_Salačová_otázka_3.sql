dfdded5c6f42d75256f19e5c8b95c75efe9cee0e
Denisa Salacova <denisa.salacova@gmail.com>
2023-11-03 20:08:17 +0100

    dokončení otázky 3

--- Projekt_SQL_Salačová_otázka_3.sql
+++ Projekt_SQL_Salačová_otázka_3.sql
@@ -7,24 +7,11 @@ SELECT*
 FROM czechia_price_category cpc 
 
 
-
-SELECT
-	cp.category_code AS kód_kategorie,
-	cpc.name AS kategorie,
-	SUM (cp.value) AS cena_za_rok,
-	EXTRACT(YEAR FROM date_from) AS rok
-FROM czechia_price cp 
-JOIN czechia_price_category cpc 
-	ON cp.category_code =cpc.code 
-GROUP BY cp.category_code, EXTRACT(YEAR FROM cp.date_from)
-ORDER BY cpc.code, cp.date_from ASC ;
-
-
 CREATE TABLE t_potraviny_roční_ceny AS
 	SELECT
 	cp.category_code AS kód_kategorie,
 	cpc.name AS kategorie,
-	SUM (cp.value) AS cena_za_rok,
+	ROUND(AVG(cp.value),2) AS průměr_ceny,
 	EXTRACT(YEAR FROM date_from) AS rok
 FROM czechia_price cp 
 JOIN czechia_price_category cpc 
@@ -32,31 +19,44 @@ JOIN czechia_price_category cpc
 GROUP BY cp.category_code, EXTRACT(YEAR FROM cp.date_from)
 ORDER BY cpc.code, cp.date_from ASC;
 
+SELECT*
+FROM t_potraviny_roční_ceny tprc 
+ 
+
+CREATE TABLE t_potraviny_rocni_rust_cen_v_procentech AS
+	WITH t_potraviny_roční_ceny AS (
+	  SELECT
+	    cp.category_code AS kód_kategorie,
+	    cpc.name AS kategorie,
+	    EXTRACT(YEAR FROM cp.date_from) AS rok,
+	    cp.value AS ceny,
+	    SUM(cp.value) AS cena_za_rok
+	  FROM czechia_price cp
+	  JOIN czechia_price_category cpc ON cp.category_code = cpc.code
+	  GROUP BY cp.category_code, EXTRACT(YEAR FROM cp.date_from)
+	)
+	SELECT
+	  tprc.kód_kategorie,
+	  tprc.kategorie,
+	  tprc.rok,
+	  tprc.ceny,
+	  tprc.cena_za_rok,
+	  LAG(tprc.cena_za_rok) OVER (PARTITION BY tprc.kód_kategorie ORDER BY tprc.rok) AS cena_za_predchozi_rok,
+	  CASE
+	    WHEN LAG(tprc.cena_za_rok) OVER (PARTITION BY tprc.kód_kategorie ORDER BY tprc.rok) IS NULL THEN 0
+	    ELSE ROUND(((tprc.cena_za_rok - LAG(tprc.cena_za_rok) OVER (PARTITION BY tprc.kód_kategorie ORDER BY tprc.rok)) / LAG(tprc.cena_za_rok) OVER (PARTITION BY tprc.kód_kategorie ORDER BY tprc.rok) * 100), 2)
+	  END AS procentualni_rust
+	FROM t_potraviny_roční_ceny tprc;
 
 
 SELECT*
-FROM t_potraviny_roční_ceny tprc ;
- 
+FROM t_potraviny_rocni_rust_cen_v_procentech tprrcvp ;
 
 
-WITH t_potraviny_roční_ceny AS (
-  SELECT
-    cp.category_code AS kód_kategorie,
-    cpc.name AS kategorie,
-    EXTRACT(YEAR FROM cp.date_from) AS rok,
-    SUM(cp.value) AS cena_za_rok
-  FROM czechia_price cp
-  JOIN czechia_price_category cpc ON cp.category_code = cpc.code
-  GROUP BY cp.category_code, EXTRACT(YEAR FROM cp.date_from)
-)
 SELECT
-  tprc.kód_kategorie,
-  tprc.kategorie,
-  tprc.rok,
-  tprc.cena_za_rok,
-  LAG(tprc.cena_za_rok) OVER (PARTITION BY tprc.kód_kategorie ORDER BY tprc.rok) AS cena_za_predchozi_rok,
-  CASE
-    WHEN LAG(tprc.cena_za_rok) OVER (PARTITION BY tprc.kód_kategorie ORDER BY tprc.rok) IS NULL THEN 0
-    ELSE ROUND(((tprc.cena_za_rok - LAG(tprc.cena_za_rok) OVER (PARTITION BY tprc.kód_kategorie ORDER BY tprc.rok)) / LAG(tprc.cena_za_rok) OVER (PARTITION BY tprc.kód_kategorie ORDER BY tprc.rok) * 100), 2)
-  END AS procentualni_rust
-FROM t_potraviny_roční_ceny tprc;
+	kód_kategorie ,
+	kategorie ,
+	SUM(procentualni_rust) AS součet_nárůstu
+FROM t_potraviny_rocni_rust_cen_v_procentech tprrcvp
+GROUP BY kategorie
+ORDER BY součet_nárůstu ASC;
