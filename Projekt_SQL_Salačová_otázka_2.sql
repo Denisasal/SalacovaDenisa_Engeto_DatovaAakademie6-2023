fd84b0d2dd50928b95133c59ea6c66d12813d1a0
Denisa Salacova <denisa.salacova@gmail.com>
2023-11-03 20:07:22 +0100

    dokončení otázky 2

--- Projekt_SQL_Salačová_otázka_2.sql
+++ Projekt_SQL_Salačová_otázka_2.sql
@@ -39,47 +39,15 @@ SELECT*
 FROM czechia_payroll cp 
 ORDER BY payroll_year DESC;
 
-SELECT
-    value AS cena,
-    category_code AS kód_kategorie,
-    date_format(date_from, '%y-%m-%d') AS období_od,
-    date_format(date_to, '%y-%m-%d') AS období_do,
-    (CASE
-        WHEN MONTH(date_from) BETWEEN 01 AND 03 THEN 1
-        WHEN MONTH(date_from) BETWEEN 04 AND 06 THEN 2
-        WHEN MONTH(date_from) BETWEEN 07 AND 09 THEN 3
-        WHEN MONTH(date_from) BETWEEN 10 AND 12 THEN 4
-        ELSE NULL
-    END) AS kvartál
-FROM czechia_price cp
-WHERE category_code = 111301 
-ORDER BY date_from ASC;
-
 
 SELECT
-   category_code AS chléb,
-	CEIL(AVG(value)) AS průměrná_cena 
-FROM czechia_price cp 
-WHERE category_code = 111301 AND date_from = '06-01-02' AND '06-12-24'
+	tmrrc.rok ,
+	tprrc.kategorie,
+	ROUND(AVG(tmrrc.průměr_mzdy),2) AS celkem_mzdy,
+	tprrc.průměr_ceny,
+	ROUND(((AVG(tmrrc.průměr_mzdy))/tprrc.průměr_ceny ),2) AS celkem_nákup
+FROM t_mzdy_rocni_rust_cen tmrrc 
+JOIN t_potraviny_rocni_rust_cen tprrc ON tmrrc.rok = tprrc.rok 
+WHERE tprrc.kód_kategorie = 111301 OR tprrc.kód_kategorie = 114201
+GROUP BY tmrrc.rok, tprrc.kategorie  ;
 
-
-SELECT
-	payroll_year AS rok,
-	SUM (value) AS součet_mzdy
-FROM czechia_payroll cp 
-WHERE payroll_year = 2006 AND calculation_code = 200 AND value_type_code = 5958
-ORDER BY payroll_year ASC;
-
-
-SELECT 1686698/15
-
--Výsledek- 112 446,5 kg chleba--
-
-
-SELECT
-   category_code AS mléko,
-	CEIL(AVG(value)) AS průměrná_cena 
-FROM czechia_price cp 
-WHERE category_code = 114201 AND date_from = '06-01-02' AND '06-12-24'
-
-SELECT 3317691/15
